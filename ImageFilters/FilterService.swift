e85ad0c78ff2aab25aedaf9187eabe16d3c55d2f
bradleypj@icloud.com <bradleypj@icloud.com>
2015-04-07 15:09:08 -0700

    first commit. added git ignore

---
+++ ImageFilters/FilterService.swift
@@ -0,0 +1,37 @@
+//
+//  FilterService.swift
+//  ImageFilters
+//
+//  Created by Bradley Johnson on 4/6/15.
+//  Copyright (c) 2015 BPJ. All rights reserved.
+//
+
+import UIKit
+import CoreImage
+
+class FilterService {
+  
+  class func sepia(originalImage : UIImage) -> UIImage {
+    
+    let image = CIImage(image: originalImage)
+    let sepia = CIFilter(name: "CISepiaTone")
+    sepia.setDefaults()
+    //sepia.setValue(1.0, forKey: kCIInputIntensityKey)
+    sepia.setValue(image, forKey: kCIInputImageKey)
+    let result = sepia.valueForKey(kCIOutputImageKey) as CIImage
+    
+    for input in sepia.inputKeys() {
+      println(input)
+    }
+
+    let options = [kCIContextWorkingColorSpace : NSNull()]
+    let eaglContext = EAGLContext(API: EAGLRenderingAPI.OpenGLES2)
+    let context = CIContext(EAGLContext: eaglContext, options: options)
+    
+    let resultRef = context.createCGImage(result, fromRect: result.extent())
+    return UIImage(CGImage: resultRef)!
+    
+    
+    
+  }
+}
---
+++ PhotoViewController.swift
@@ -0,0 +1,82 @@
+//
+//  PhotoViewController.swift
+//  ImageFilters
+//
+//  Created by Bradley Johnson on 4/6/15.
+//  Copyright (c) 2015 BPJ. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  
+   let alertController = UIAlertController(title: "Options", message: "Choose One", preferredStyle: UIAlertControllerStyle.ActionSheet)
+  
+  let imageToUploadSize = CGSize(width: 400, height: 400)
+
+  @IBOutlet weak var primaryImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      self.title = "Upload"
+      
+      if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
+        let cameraAction = UIAlertAction(title: "Camera", style: UIAlertActionStyle.Default) { (action) -> Void in
+          
+          let imagePickerController = UIImagePickerController()
+          imagePickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+          imagePickerController.allowsEditing = true
+          self.presentViewController(imagePickerController, animated: true, completion: nil)
+          imagePickerController.delegate = self
+          //imagePickerController.mediaTypes = [UIImagePickerControllerMediaType]
+        }
+        self.alertController.addAction(cameraAction)
+      }
+      
+      let sepiaAction = UIAlertAction(title: "Sepia", style: UIAlertActionStyle.Destructive) { (action) -> Void in
+        
+        let sepiaImage = FilterService.sepia(self.primaryImageView.image!)
+        self.primaryImageView.image = sepiaImage
+      }
+      self.alertController.addAction(sepiaAction)
+      
+      let uploadAction = UIAlertAction(title: "Upload", style: UIAlertActionStyle.Default) { (action) -> Void in
+        
+        ParseService.uploadImage(self.primaryImageView.image!, size: self.imageToUploadSize, completionHandler: { (errorDescription) -> Void in
+          
+        })
+      }
+      self.alertController.addAction(uploadAction)
+
+        // Do any additional setup after loading the view.
+    }
+
+  @IBAction func photoButtonPressed(sender: AnyObject) {
+    
+    if let popoverController = self.alertController.popoverPresentationController {
+      if let button = sender as? UIButton {
+        popoverController.sourceView = button
+        popoverController.sourceRect = button.bounds
+      }
+    }
+    
+    self.presentViewController(alertController, animated: true) { () -> Void in
+      
+    }
+  }
+
+  //MARK:
+  //MARK: UIImagePickerControllerDelegate
+  
+  func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+    if let editedImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+      self.primaryImageView.image = editedImage
+    }
+    
+    picker.dismissViewControllerAnimated(true, completion: nil)
+  }
+  
+  func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+    
+  }
+
+}
